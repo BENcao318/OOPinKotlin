99663c728e153dfa7103c80905145240035455a8
ben3617 <cby204@gmail.com>
2024-01-01 22:08:43 -0500

    Inheritance

--- app/src/main/java/com/bencao/oopinkotlin/MySecondCars.kt
+++ app/src/main/java/com/bencao/oopinkotlin/MySecondCars.kt
@@ -4,7 +4,6 @@ class MySecondCars {
     var name:String? = null
     var model:Int? = null
         private set
-
     constructor(name: String?, model: Int?) {
         this.name = name
         this.model = model
